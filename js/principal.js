80c49f37afa06bd8b272cb26720d4a7abc603d25
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-05-11 12:20:02 -0300

    implementado calculo imc via dom/implemented bmi calculation by dom

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,17 @@ var titulo = document.querySelector(".titulo");
 var novoTitulo = "Aparecida Nutricionista"
 titulo.textContent = novoTitulo;
 document.title = novoTitulo;
+
+var trPaciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = trPaciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = trPaciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = trPaciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+console.log(imc);
