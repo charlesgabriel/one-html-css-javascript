12dda0bbec62717972bb386aafb7a5527cbae15e
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-05-12 11:23:22 -0300

    implementado recurso para adicionar na tabela/implemented feature to add to table

--- js/principal.js
+++ js/principal.js
@@ -7,6 +7,9 @@ function query(item, id) {
 function queryContent(item, id) {
     return item.querySelector(id).textContent;
 }
+function create(element) {
+    return document.createElement(element);
+}
 
 var titulo = query(document, ".titulo");
 var novoTitulo = "Aparecida Nutricionista"
@@ -46,5 +49,31 @@ for (let i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = query(document, "#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    
+    var form = query(document, "#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = create("tr");
+    var nomeTd = create("td");
+    var pesoTd = create("td");
+    var alturaTd = create("td");
+    var gorduraTd = create("td");
+    var imcTd = create("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = 0;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = query(document, "#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 });
